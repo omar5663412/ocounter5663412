7522ccfe3e4c9ad56720c496e8e3268aae4897de
omar5663412 <omar5663412@gmail.com>
2021-03-20 17:37:54 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import './App.css';
 
 export default function App() {
   return (
@@ -9,3 +10,20 @@ export default function App() {
     </div>
   );
 }
+
+class  App extends Component {
+  fileSelectedHandler = event => {
+    console.log(event.target)
+  }
+  render() {
+    return (
+      <div className="App">
+      <input type="file" onChange={this.fileSelectedHandler}/>
+    <div>
+    );
+  }
+}
+
+
+
+export defualt  App;
